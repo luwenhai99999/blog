287965f9571dd4b350ab955a86c611000fcf9511
luwenhai <L834299862@outlook.com>
2022-07-23 21:20:58 +0800

    1.0.0

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+
+const { defaultTheme }  = require('@vuepress/theme-default')
+
+module.exports = {
+  base: '/Blog',
+  lang: 'zh-CN',
+  title: 'hai ！',
+  description: '记录一些， 分享一些',
+  head:[['link',{rel: 'icon', href: '/sun.png' }]],
+  theme: defaultTheme({
+    navbar:[
+      { text: '首页', link: '/view/home/' },
+      { text: 'Vue', link: '/view/vue' },
+      { text: 'js', link: '/view/js' },
+    ],
+    logo:'/sun.png',
+
+    
+    
+  })
+}
